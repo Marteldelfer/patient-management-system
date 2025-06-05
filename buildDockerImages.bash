40bb35c57f8d2c8b47c4c856f3997c611aff8cbf
Marteldelfer <marteldelfer@gmail.com>
2025-06-05 14:12:46 -0300

    config api-gateway

--- buildDockerImages.bash
+++ buildDockerImages.bash
@@ -1,7 +1,4 @@
-cd ./patient-service
-docker build -t patient-service .
-cd ../billing-service
-docker build -t billing-service .
-cd ../analytics-service
-docker build -t analytics-service .
-cd ..
+docker build -t patient-service ./patient-service
+docker build -t billing-service ./billing-service
+docker build -t analytics-service ./analytics-service
+docker build -t api-gateway ./api-gateway
