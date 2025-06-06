79e839bccd6e6976921d304d2536feefe8042e1d
Marteldelfer <marteldelfer@gmail.com>
2025-06-05 23:17:49 -0300

    handle jwt in auth service

--- buildDockerImages.bash
+++ buildDockerImages.bash
@@ -2,3 +2,4 @@ docker build -t patient-service ./patient-service
 docker build -t billing-service ./billing-service
 docker build -t analytics-service ./analytics-service
 docker build -t api-gateway ./api-gateway
+docker build -t auth-service ./auth-service
